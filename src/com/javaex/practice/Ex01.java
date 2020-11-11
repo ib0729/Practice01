fcf95a315f9d9dd7d50922d313879a4bed1652d6
'ib0729' <'ib0729@naver.com'>
2020-11-11 18:13:28 +0900

    제출

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int myAge;
+		int _of_workers;
+		int countOfLetterslnString;
+		int stLevel1;
+		int person;
+		int _person;
+	}
+
+}
---
+++ src/com/javaex/practice/Ex10.java
@@ -0,0 +1,15 @@
+package com.javaex.practice;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int x=5;                         //정의를 안지어줘서//
+		System.out.println(x);           //x=5다 x=3이다 이런식으로//
+		
+		
+	}
+
+}
